181d843e54655fb37108662b6186d8a01f8b1e40
breannams <breannamarie_s@hotmail.com>
2021-01-30 21:56:52 -0500

    Finish Api class

--- lib/Cleveland_Museum_of_Art_CLI/api.rb
+++ lib/Cleveland_Museum_of_Art_CLI/api.rb
@@ -1,5 +1,4 @@
-##cares about getting requests and sending/formating them for artwork class
-##in art the label("Aggregated information about the artwork.") for the artwork is called a tombstone :)
+
 require "httparty"
 require_relative "./artwork"
 # require "pry"
@@ -11,25 +10,21 @@ class Api
     def get_artwork
           url = "https://openaccess-api.clevelandart.org/api/artworks/"
           tombstone = HTTParty.get(url)
-          # tombstone.each do |h,v|
-          #    ##get access to title,series, etc. in a hash
-          #   end
-          artworks_hash = {
-            title: tombstone["data"].first["title"],
-            series: tombstone["data"].first["series"],
-            creators: tombstone["data"].first["creators"].first["description"],
-            creator_bio: tombstone["data"].first["creators"].first["biography"],
-            culture: tombstone["data"].first["culture"],
-            department: tombstone["data"].first["department"],
-            type: tombstone["data"].first["type"],
-            fun_fact: tombstone["data"].first["fun_fact"],
-        
-      }
-     artwork = Artwork.new(artworks_hash)
+            tombstone["data"].each do |art|
+              artwork_hash = {
+                title: art["title"],
+                series: art["series"],
+                creators: art["description"],
+                creator_bio: art["biography"],
+                culture: art["culture"],
+                department: art["department"],
+                type: art["type"],
+                fun_fact: art["fun_fact"]
+              }
+              Artwork.new(artwork_hash)
+            end
     
-     puts Artwork.all
     end
-
 
 end
 
@@ -38,17 +33,3 @@ end
  api = Api.new
  api.get_artwork
 
-##available keys from url hash = info, data
-## data = a hash and seems like it has all the info we need!! yay!!
-## data.first.keys returns a buncha keys, want to use just these for this app i think !!!::
-
-## title
-## series
-## creation_date
-## creators ----> ALSO HAS A BIOGRAPHY??!
-## culture
-## department
-## fun_fact
-## digital_description
-## wall_description
-## type
